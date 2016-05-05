89b2818839aa69199cbba9e6f76eee7f5911cdfe
Brandon Moody <brandon@thef33d.com>
2016-05-05 01:29:38 -0700

    initial push

---
+++ app.js
@@ -0,0 +1,19 @@
+(function() {
+  'use strict';
+  function App() {
+    var urlPort = '80';
+    var urlPrefix = 'ec2-54-186-243-248.us-west-2';
+    var apiHost = 'http://' + urlPrefix + '.compute.amazonaws.com:' + urlPort;
+
+    var authCode = null;
+
+    var service = {
+    };
+
+    return service;
+
+  }
+  //window.StatsService = StatsService();
+})();
+
+
